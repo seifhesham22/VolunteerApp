95f62091f3fd26908d68154cc09b43c9e12cd54b
Seif Elmoazen <seifhesham882@gmail.com>
2026-02-21 23:21:31 +0700

    -Defined Classes to automate Gamefication - Made the service type to be class intead of enum - Introduced Templates - Added VolunteerProfile

---
+++ VolunteerApp.Domain/Profiles/StudentProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VolunteerApp.Domain.Abstractions;
+using VolunteerApp.Domain.Entities;
+
+namespace VolunteerApp.Domain.Profiles
+{
+    public class StudentProfile : BaseEntity
+    {
+        public Guid UserId { get; private set; }
+        public string Bio { get; private set; } = string.Empty;
+    }
+}
